4ab2c6fd8827c215000a276161938452de5069b7
yoriba <yoriba@gmail.com>
2024-03-30 22:07:55 +0900

    interface and futureETHPool
    interface
    futureETHPool
    
    - 기존 샘플코드 기준으로 포크작업
    - 버전 변경 및 오류수정

---
+++ packages/hardhat/contracts/Interfaces.sol
@@ -0,0 +1,34 @@
+//SPDX-License-Identifier: MIT
+
+ pragma solidity 0.8.17;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorInterface.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router01.sol";
+
+
+interface ILiquidityPool {
+    event Withdraw(
+        address indexed account,
+        uint256 amount,
+        uint256 writeAmount
+    );
+
+    event Provide(address indexed account, uint256 amount, uint256 writeAmount);
+    function lock(uint256 amount) external;
+    function unlock(uint256 amount) external;
+    function unlockPremium(uint256 amount) external;
+    function send(address payable account, uint256 amount) external;
+    function setLockupPeriod(uint value) external;
+    function totalBalance() external view returns (uint256 amount);
+}
+
+
+interface IERCLiquidityPool is ILiquidityPool {
+    function sendPremium(uint256 amount) external;
+    function token() external view returns (IERC20);
+}
+
+interface IETHLiquidityPool is ILiquidityPool {
+    function sendPremium() external payable;
+}
